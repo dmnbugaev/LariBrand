7deb72ca1e3a69968c16f04b68036c16b9efc79e
dmnbugaev <dmnbugaev@gmail.com>
2026-01-20 19:32:57 +0400

    добавил мужские стрижки

--- src/app/hair_cutting/page.jsx
+++ src/app/hair_cutting/page.jsx
@@ -10,7 +10,7 @@ export default function Hair_Cutting_Page() {
         <>
             <Header />
             <Hair_Cutting />
-            <BeforeAfterGallery />
+            <BeforeAfterGallery defaultCategory="Стрижка волос" />
             <Reviews />
             <Footer />
         </>
