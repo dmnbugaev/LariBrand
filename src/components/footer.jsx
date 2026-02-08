cf8c2ecff4177714b6e4ae87410d844ecdda7053
dmnbugaev <dmnbugaev@gmail.com>
2026-02-08 23:38:24 +0400

    убрал кнопку ватсап и кнопки из футера

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -42,36 +42,6 @@ export default function Footer() {
                         <a href={content.phone_number_2_link}>{content.phone_number_2}</a>
                         <a href={content.phone_number_1_link}>{content.phone_number_1}</a>
                     </div>
-                    <div className="footer-btn">                        
-                        <UI_kit_link_footer href={content.telegram_link}>
-                            <Image
-                                src="/icons/footer-icon/tg.svg" 
-                                alt="Telegram LariBrand"
-                                width={34}      
-                                height={34}   
-                                className='btn-tg-footer'  
-                            />
-                        </UI_kit_link_footer>
-
-                        <UI_kit_link_footer href={content.vk_link}>
-                            <Image
-                                src="/icons/footer-icon/vk.svg" 
-                                alt="VK LariBrand"
-                                width={40}      
-                                height={40}     
-                            />
-                        </UI_kit_link_footer>
-
-                        <UI_kit_link_footer href={content.whatsapp_link}>
-                            <Image
-                                src="/icons/footer-icon/wh1.svg" 
-                                alt="WhatsApp LariBrand"
-                                width={38}      
-                                height={38}   
-                                className='btn-wh-footer'  
-                            />
-                        </UI_kit_link_footer>
-                    </div>
                 </div>
             </div>
         </footer>
