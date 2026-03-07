2e9c3858496d4ee3d972ec16c5423f397836a538
dmnbugaev <dmnbugaev@gmail.com>
2026-03-07 14:14:05 +0400

    обновил сайт

--- src/components/services/AdditionalServices/AdditionalServicesComponent.tsx
+++ src/components/services/AdditionalServices/AdditionalServicesComponent.tsx
@@ -2,5 +2,5 @@ import ServicePage from '../ServicePage'
 import content from '../../../../content/content.json'
 
 export default function AdditionalServicesComponent() {
-  return <ServicePage data={content.additional_services} htmlTitles={true} />
+  return <ServicePage data={content.additional_services} />
 }
