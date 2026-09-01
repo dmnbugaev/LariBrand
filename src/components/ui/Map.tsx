f7464817d01e5795933b550c331901c23da8c5ab
dmnbugaev <dmnbugaev@gmail.com>
2026-09-01 15:40:07 +0300

    обновил карту

--- src/components/ui/Map.tsx
+++ src/components/ui/Map.tsx
@@ -1,35 +1,16 @@
-'use client'
-
-import { useEffect, useRef } from 'react'
+const YANDEX_MAP_URL =
+  'https://yandex.ru/map-widget/v1/?ll=46.029315%2C51.522283&mode=search&oid=103694209198&ol=biz&z=16'
 
 export default function Map() {
-  const containerRef = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    const mapContainer = containerRef.current
-    if (!mapContainer) return
-
-    mapContainer.innerHTML = ''
-
-    const script = document.createElement('script')
-    script.type = 'text/javascript'
-    script.charset = 'utf-8'
-    script.async = true
-    script.src =
-      'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Aa4c44388f23506ce84cd0c42145a6cc5ab329a6cb6819afe9a3f7ae9e6367c4c&width=100%25&height=380&lang=ru_RU&scroll=false'
-
-    mapContainer.appendChild(script)
-
-    return () => {
-      mapContainer.innerHTML = ''
-    }
-  }, [])
-
   return (
-    <div
-      ref={containerRef}
-      className="h-[380px] w-full border border-brand-black"
-      aria-label="Карта проезда в LariBrand"
-    />
+    <div className="h-[380px] w-full overflow-hidden border border-brand-black">
+      <iframe
+        src={YANDEX_MAP_URL}
+        title="Карта проезда в LariBrand"
+        className="h-full w-full border-0"
+        loading="lazy"
+        allowFullScreen
+      />
+    </div>
   )
 }
